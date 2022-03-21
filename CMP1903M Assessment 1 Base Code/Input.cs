4457f04aaa52e4c3152c42e790804649decf5d97
FirstClassGravy <41171656+FirstClassGravy@users.noreply.github.com>
2022-03-21 21:20:28 +0000

    Add project files.

---
+++ CMP1903M Assessment 1 Base Code/Input.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    public class Input
+    {
+
+        //Handles the text input for Assessment 1
+        string text = " ";
+        List<string> textInput = new List<string>();
+
+        //Method: manualTextInput
+        //Arguments: none
+        //Returns: string
+        //Gets text input from the keyboard
+        public List<string> ManualTextInput()
+        {
+            do
+            {
+                Console.WriteLine("Enter a sentence or press * to finish: ");
+                text = Console.ReadLine();
+                if (text == "")
+                {
+                    Console.WriteLine("You forgot to enter the sentence!");
+                }
+                else
+                {
+                    this.textInput.Add(text)
+                }
+            }
+            while (text.EndsWith("*"));
+            return textInput;
+        }
+
+        //Method: fileTextInput
+        //Arguments: string (the file path)
+        //Returns: string
+        //Gets text input from a .txt file
+        public string FileTextInput(string fileName)
+        {
+            return textList;
+        }
+
+    }
+}
